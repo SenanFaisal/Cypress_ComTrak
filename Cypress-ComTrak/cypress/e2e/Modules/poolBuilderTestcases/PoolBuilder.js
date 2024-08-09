181f5cdd9e822754537beff2b19639d2e9dfbf95
Senan <1senan.faisal@gmail.com>
2024-08-09 16:30:55 +0100

    initial changes

---
+++ Cypress-ComTrak/cypress/e2e/Modules/poolBuilderTestcases/PoolBuilder.js
@@ -0,0 +1,346 @@
+// import PoolBuilderSelectors from '../../Selectors/PoolBuilderSelectors'
+
+// class PoolBuilder {
+//   PublishPatientPool() {
+//     cy.visit('https://comtrak.qa.dmclinical.com/')
+//     cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//       { args: PoolBuilderSelectors },
+//       ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//       ) => {
+//         cy.wait(5000)
+//         cy.contains(' Sign In').click()
+//         cy.wait(5000)
+//         cy.get(DRP_Leads).click()
+//         cy.wait(2000)
+//         cy.contains('Patient Pool Builder ').click()
+//         cy.wait(2000)
+//         cy.get(BTN_Create).click()
+//         cy.wait(2000)
+//         cy.get(TXT_PoolName)
+//           .type(Pool_Name)
+//         cy.get(TXT_Description)
+//           .type(Description)
+//         cy.get(BTN_Continue1).click()
+//         cy.wait(2000)
+//         cy.contains('Select State').click()
+//         cy.wait(2000)
+//         cy.contains('New York').click()
+//         cy.contains('New Jersey').click()
+//         cy.wait(2000)
+//         cy.contains('Select Status').click()
+//         cy.wait(2000)
+//         cy.contains('Pre Booking').click()
+//         cy.wait(2000)
+//         cy.get('.ngx-slider-pointer-min')
+//           .focus()
+//           .type('{rightArrow}'.repeat(6))
+//         cy.get('.ngx-slider-pointer-max')
+//           .focus()
+//           .type('{leftArrow}'.repeat(2))
+//         cy.wait(2000)
+//         cy.get(BTN_Continue2).contains(' Continue ').click()
+//         cy.wait(2000)
+//         cy.get(DRP_IntendedStudy).click({ force: true })
+//         cy.wait(2000)
+//         cy.get(IntendedStudy).click({ force: true })
+//         cy.wait(2000)
+//         cy.get(BTN_CreatePool).click()
+//         cy.wait(2000)
+//         cy.get('tr:nth-child(1) > td:nth-child(3)').should('contain', Pool_Name)
+//         cy.wait(1000)
+//         cy.get(EYE_ReviewPool).click()
+//         cy.get('[id="container "]').should('contain', ' Review Patients Pool ')
+//         cy.wait(1000)
+//         cy.get(Verify_Name).should('contain', Pool_Name)
+//         cy.get(Verify_Description).should('contain', Description)
+//         cy.get(Verify_State1).should('contain', ' New York ')
+//         cy.get(Verify_State2).should('contain', ' New Jersey ')
+//         cy.get(Verify_Status).should('contain', ' Pre Booking ')
+//         cy.get(Verify_Age).should('contain', '6 years - 98 years')
+//         cy.get(Verify_Study).should('contain', ' Moderna 1273-P301 ')
+//         cy.wait(2000)
+//         cy.get(BTN_Cross).click()
+//         cy.wait(1000)
+//       })
+//   }
+
+//   SavePatientPool() {
+//     cy.visit('https://comtrak.qa.dmclinical.com/')
+//     cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//       { args: PoolBuilderSelectors },
+//       ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//       ) => {
+//         cy.wait(5000)
+//         cy.contains(' Sign In').click()
+//         cy.wait(5000)
+//         cy.get(DRP_Leads).click()
+//         cy.wait(2000)
+//         cy.contains('Patient Pool Builder ').click()
+//         cy.wait(2000)
+//         cy.get(BTN_Create).click()
+//         cy.wait(2000)
+//         cy.get(TXT_PoolName)
+//           .type(Pool_Name)
+//         cy.get(TXT_Description)
+//           .type(Description)
+//         cy.get(BTN_Continue1).click()
+//         cy.wait(2000)
+//         cy.contains('Select State').click()
+//         cy.wait(2000)
+//         cy.contains('New York').click()
+//         cy.contains('New Jersey').click()
+//         cy.wait(2000)
+//         cy.contains('Select Status').click()
+//         cy.wait(2000)
+//         cy.contains('Pre Booking').click()
+//         cy.wait(2000)
+//         cy.get('.ngx-slider-pointer-min')
+//           .focus()
+//           .type('{rightArrow}'.repeat(6))
+//         cy.get('.ngx-slider-pointer-max')
+//           .focus()
+//           .type('{leftArrow}'.repeat(2))
+//         cy.wait(2000)
+//         cy.get(BTN_Continue2).contains(' Continue ').click()
+//         cy.wait(2000)
+//         cy.get(DRP_IntendedStudy).click({ force: true })
+//         cy.wait(2000)
+//         cy.get(IntendedStudy).click({ force: true })
+//         cy.wait(2000)
+//         cy.get(BTN_Review).click()
+//         cy.wait(1000)
+//         cy.get(Verify_Name).should('contain', Pool_Name)
+//         cy.get(Verify_Description).should('contain', Description)
+//         cy.get(Verify_State1).should('contain', ' New York ')
+//         cy.get(Verify_State2).should('contain', ' New Jersey ')
+//         cy.get(Verify_Status).should('contain', ' Pre Booking ')
+//         cy.get(Verify_Age).should('contain', '6 years - 98 years')
+//         // cy.get(Verify_Study).should('contain', ' Moderna 1273-P301 ')
+//         cy.wait(2000)
+//         cy.get(BTN_Cross).click()
+//         cy.wait(1000)
+//         cy.get(BTN_SaveDraft).contains(' Save as draft ').click()
+//         cy.wait(1000)
+//         cy.get(BTN_Drafts).click()
+//         cy.wait(1000)
+//         cy.get('tr:nth-child(1) > td:nth-child(2)').should('contain', Pool_Name)
+//         cy.wait(1000)
+//       })
+//   }
+
+//   // DiscardPatientPool() {
+//   //   cy.visit('https://comtrak.qa.dmclinical.com/')
+//   //   cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//   //     { args: PoolBuilderSelectors },
+//   //     ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//   //     ) => {
+//   //       cy.wait(5000)
+//   //       cy.contains(' Sign In').click()
+//   //       cy.wait(5000)
+//   //       cy.get(DRP_Leads).click()
+//   //       cy.wait(2000)
+//   //       cy.contains('Patient Pool Builder ').click()
+//   //       cy.wait(2000)
+//   //     })
+//   // }
+
+//   // ExistingPoolNameCheck() {
+//   //   cy.visit('https://comtrak.qa.dmclinical.com/')
+//   //   cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//   //     { args: PoolBuilderSelectors },
+//   //     ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//   //     ) => {
+//   //       cy.wait(5000)
+//   //       cy.contains(' Sign In').click()
+//   //       cy.wait(5000)
+//   //       cy.get(DRP_Leads).click()
+//   //       cy.wait(2000)
+//   //       cy.contains('Patient Pool Builder ').click()
+//   //       cy.wait(2000)
+//   //     })
+//   // }
+
+//   // DataValidationPool() {
+//   //   cy.visit('https://comtrak.qa.dmclinical.com/')
+//   //   cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//   //     { args: PoolBuilderSelectors },
+//   //     ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//   //     ) => {
+//   //       cy.wait(5000)
+//   //       cy.contains(' Sign In').click()
+//   //       cy.wait(5000)
+//   //       cy.get(DRP_Leads).click()
+//   //       cy.wait(2000)
+//   //       cy.contains('Patient Pool Builder ').click()
+//   //       cy.wait(2000)
+//   //     })
+//   // }
+
+//   // DuplicatePublishedPool() {
+//   //   cy.visit('https://comtrak.qa.dmclinical.com/')
+//   //   cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//   //     { args: PoolBuilderSelectors },
+//   //     ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//   //     ) => {
+//   //       cy.wait(5000)
+//   //       cy.contains(' Sign In').click()
+//   //       cy.wait(5000)
+//   //       cy.get(DRP_Leads).click()
+//   //       cy.wait(2000)
+//   //       cy.contains('Patient Pool Builder ').click()
+//   //       cy.wait(2000)
+//   //     })
+//   // }
+//   // DuplicateSavedPool() {
+//   //   cy.visit('https://comtrak.qa.dmclinical.com/')
+//   //   cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//   //     { args: PoolBuilderSelectors },
+//   //     ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//   //     ) => {
+//   //       cy.wait(5000)
+//   //       cy.contains(' Sign In').click()
+//   //       cy.wait(5000)
+//   //       cy.get(DRP_Leads).click()
+//   //       cy.wait(2000)
+//   //       cy.contains('Patient Pool Builder ').click()
+//   //       cy.wait(2000)
+//   //     })
+//   // }
+//   // DeleteFromDrafts() {
+//   //   cy.visit('https://comtrak.qa.dmclinical.com/')
+//   //   cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//   //     { args: PoolBuilderSelectors },
+//   //     ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//   //     ) => {
+//   //       cy.wait(5000)
+//   //       cy.contains(' Sign In').click()
+//   //       cy.wait(5000)
+//   //       cy.get(DRP_Leads).click()
+//   //       cy.wait(2000)
+//   //       cy.contains('Patient Pool Builder ').click()
+//   //       cy.wait(2000)
+//   //     })
+//   // }
+//   // EditFromDrafts() {
+//   //   cy.visit('https://comtrak.qa.dmclinical.com/')
+//   //   cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//   //     { args: PoolBuilderSelectors },
+//   //     ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//   //     ) => {
+//   //       cy.wait(5000)
+//   //       cy.contains(' Sign In').click()
+//   //       cy.wait(5000)
+//   //       cy.get(DRP_Leads).click()
+//   //       cy.wait(2000)
+//   //       cy.contains('Patient Pool Builder ').click()
+//   //       cy.wait(2000)
+//   //     })
+//   // }
+//   // ReviewFromDrafts() {
+//   //   cy.visit('https://comtrak.qa.dmclinical.com/')
+//   //   cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//   //     { args: PoolBuilderSelectors },
+//   //     ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//   //     ) => {
+//   //       cy.wait(5000)
+//   //       cy.contains(' Sign In').click()
+//   //       cy.wait(5000)
+//   //       cy.get(DRP_Leads).click()
+//   //       cy.wait(2000)
+//   //       cy.contains('Patient Pool Builder ').click()
+//   //       cy.wait(2000)
+//   //     })
+//   // }
+//   // InactivateFromPublished() {
+//   //   cy.visit('https://comtrak.qa.dmclinical.com/')
+//   //   cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//   //     { args: PoolBuilderSelectors },
+//   //     ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//   //     ) => {
+//   //       cy.wait(5000)
+//   //       cy.contains(' Sign In').click()
+//   //       cy.wait(5000)
+//   //       cy.get(DRP_Leads).click()
+//   //       cy.wait(2000)
+//   //       cy.contains('Patient Pool Builder ').click()
+//   //       cy.wait(2000)
+//   //     })
+//   // }
+//   // ActivateFromPublished() {
+//   //   cy.visit('https://comtrak.qa.dmclinical.com/')
+//   //   cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//   //     { args: PoolBuilderSelectors },
+//   //     ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//   //     ) => {
+//   //       cy.wait(5000)
+//   //       cy.contains(' Sign In').click()
+//   //       cy.wait(5000)
+//   //       cy.get(DRP_Leads).click()
+//   //       cy.wait(2000)
+//   //       cy.contains('Patient Pool Builder ').click()
+//   //       cy.wait(2000)
+//   //     })
+//   // }
+//   // ReleaseAllPatients() {
+//   //   cy.visit('https://comtrak.qa.dmclinical.com/')
+//   //   cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//   //     { args: PoolBuilderSelectors },
+//   //     ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//   //     ) => {
+//   //       cy.wait(5000)
+//   //       cy.contains(' Sign In').click()
+//   //       cy.wait(5000)
+//   //       cy.get(DRP_Leads).click()
+//   //       cy.wait(2000)
+//   //       cy.contains('Patient Pool Builder ').click()
+//   //       cy.wait(2000)
+//   //     })
+//   // }
+//   // AssignRecruiter() {
+//   //   cy.visit('https://comtrak.qa.dmclinical.com/')
+//   //   cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//   //     { args: PoolBuilderSelectors },
+//   //     ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//   //     ) => {
+//   //       cy.wait(5000)
+//   //       cy.contains(' Sign In').click()
+//   //       cy.wait(5000)
+//   //       cy.get(DRP_Leads).click()
+//   //       cy.wait(2000)
+//   //       cy.contains('Patient Pool Builder ').click()
+//   //       cy.wait(2000)
+//   //     })
+//   // }
+//   // ReviewFromDrafts() {
+//   //   cy.visit('https://comtrak.qa.dmclinical.com/')
+//   //   cy.origin('https://comtrak.qa.dmclinical.com/',
+
+//   //     { args: PoolBuilderSelectors },
+//   //     ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
+//   //     ) => {
+//   //       cy.wait(5000)
+//   //       cy.contains(' Sign In').click()
+//   //       cy.wait(5000)
+//   //       cy.get(DRP_Leads).click()
+//   //       cy.wait(2000)
+//   //       cy.contains('Patient Pool Builder ').click()
+//   //       cy.wait(2000)
+//   //     })
+//   // }
+
+// }
+// export default new PoolBuilder
