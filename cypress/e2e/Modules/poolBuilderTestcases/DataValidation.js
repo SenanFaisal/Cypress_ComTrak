48a255a2d36ff33f34a556f1ae9150bb071f4dab
SenanFaisal <1senan.faisal@gmail.com>
2024-08-21 15:46:24 +0500

    New Updates

--- cypress/e2e/Modules/poolBuilderTestcases/DataValidation.js
+++ cypress/e2e/Modules/poolBuilderTestcases/DataValidation.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD:cypress/e2e/Modules/poolBuilderTestcases/DataValidation.js
-import PoolBuilderSelectors from '../../../Selectors/PoolBuilderSelectors.js'
-
-class DataValidation{
-    VerifyTotalAndAdjustedCount() {
-        cy.visit('https://comtrak.qa.dmclinical.com/')
-        cy.origin('https://comtrak.qa.dmclinical.com/',
-    
-          { args: PoolBuilderSelectors },
-          ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
-          ) => {
-            cy.wait(5000)
-            cy.contains(' Sign In').click()
-            cy.wait(5000)
-            cy.get(DRP_Leads).click()
-            cy.wait(2000)
-            cy.contains('Patient Pool Builder ').click()
-            cy.wait(2000)
-          })
-      }
-}
-=======
 import PoolBuilderSelectors from '../../../Selectors/PoolBuilderSelectors.js'
 
 class DataValidation {
@@ -97,5 +75,4 @@ class DataValidation {
       })
   }
 }
->>>>>>> Dev:Cypress-ComTrak/cypress/e2e/Modules/poolBuilderTestcases/DataValidation.js
 export default new DataValidation
--- cypress/e2e/Modules/poolBuilderTestcases/ExistingNameCheck.js
+++ cypress/e2e/Modules/poolBuilderTestcases/ExistingNameCheck.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD:cypress/e2e/Modules/poolBuilderTestcases/ExistingNameCheck.js
-import PoolBuilderSelectors from '../../../Selectors/PoolBuilderSelectors.js'
-
-class ExistingNameCheck{
-  //Verify the pool with existing name cannot be created
-    ExistingPoolNameCheck() {
-        cy.visit('https://comtrak.qa.dmclinical.com/')
-        cy.origin('https://comtrak.qa.dmclinical.com/',
-    
-          { args: PoolBuilderSelectors },
-          ({ DRP_Leads, TXT_PoolName, Pool_Name, BTN_Create, TXT_Description, Description, BTN_Continue1, BTN_Continue2, DRP_IntendedStudy, IntendedStudy, BTN_Review, Verify_Name, Verify_Description, Verify_State1, Verify_State2, Verify_Status, Verify_Age, Verify_Study, BTN_Cross, BTN_SaveDraft, BTN_Drafts, BTN_Published, BTN_CreatePool, EYE_ReviewPool }
-          ) => {
-            cy.wait(5000)
-            cy.contains(' Sign In').click()
-            cy.wait(5000)
-            cy.get(DRP_Leads).click()
-            cy.wait(2000)
-            cy.contains('Patient Pool Builder ').click()
-            cy.wait(2000)
-          })
-      }
-}
-=======
 import PoolBuilderSelectors from '../../../Selectors/PoolBuilderSelectors.js'
 
 class ExistingNameCheck {
@@ -50,5 +27,4 @@ class ExistingNameCheck {
       })
   }
 }
->>>>>>> Dev:Cypress-ComTrak/cypress/e2e/Modules/poolBuilderTestcases/ExistingNameCheck.js
 export default new ExistingNameCheck
