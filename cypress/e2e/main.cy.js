eb2053c340d9af3cd38213eda20d134fb2b9e799
Senan Faisal <senanf.mat@dmclinical.com>
2024-10-15 15:16:17 +0500

    Testing CI/CD

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,16 +1,15 @@
 describe("Innova Inspection", () => {
   it("Login to the App", () => {
     cy.visit("https://black-sky-03aa5f910.5.azurestaticapps.net/");
-    // cy.xpath('//a[text()="Go to Home"]').click();
     cy.xpath('//input[@id="email"]').type(
-      ""
+      "hozefa.tankiwala@matechnologies.net"
     );
     cy.wait(2000);
     cy.xpath('//button[@type="submit"]').click();
     cy.wait(2000);
     cy.xpath('//span[text()="Select"]').click();
     cy.xpath('//li[text()="dev-db"]').click();
-    cy.xpath('//input[@id="password"]').type("");
+    cy.xpath('//input[@id="password"]').type("zaq1ZAQ!");
     cy.xpath('//button[@type="submit"]').click();
     cy.wait(2000);
     cy.xpath('//button[text()="Scheduled"]').click();
