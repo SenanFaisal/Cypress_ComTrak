f49094d3d0e9abe4a289747c8e3781370f736ab1
Senan Faisal <senanf.mat@dmclinical.com>
2024-10-15 12:11:10 +0500

    Innova Inspection Test

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,23 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
+  "chromeWebSecurity": false,
+  "pageLoadTimeout": 250000,
+  "experimentalSessionAndOrigin": true,
+  "defaultCommandTimeout": 10000,
   viewportWidth: 430,
   viewportHeight: 932,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on("before:browser:launch", (browser, launchOptions) => {
+        if (browser.name === "chrome") {
+          launchOptions.args.push("--disable-site-isolation-trials");
+          launchOptions.args.push("--incognito");
+          return launchOptions;
+        }
+      });
     },
   },
 });
--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,9 +1,23 @@
-describe('Innova Inspection', () => {
-  it('Login to the App', () => {
-    cy.visit('https://black-sky-03aa5f910.5.azurestaticapps.net/');
-    cy.xpath('//a[text()="Go to Home"]').click();
-    cy.xpath('//input[@id="email"]').type('hozefa.tankiwala@matechnologies.net');
+describe("Innova Inspection", () => {
+  it("Login to the App", () => {
+    cy.visit("https://black-sky-03aa5f910.5.azurestaticapps.net/");
+    // cy.xpath('//a[text()="Go to Home"]').click();
+    cy.xpath('//input[@id="email"]').type(
+      "hozefa.tankiwala@matechnologies.net"
+    );
     cy.xpath('//button[@type="submit"]').click();
-    
-  })
-})
+    cy.wait(2000);
+    cy.xpath('//span[text()="Select"]').click();
+    cy.xpath('//li[text()="dev-db"]').click();
+    cy.xpath('//input[@id="password"]').type('zaq1ZAQ!');
+    cy.xpath('//button[@type="submit"]').click();
+    cy.wait(2000);
+    cy.xpath('//button[text()="Scheduled"]').click();
+    cy.wait(2000);
+    cy.xpath('//button[text()="Completed"]').click();
+    cy.wait(2000);
+    cy.xpath('(//img[@alt="Menu Icon"])[1]');
+    cy.wait(2000);
+
+  });
+});
